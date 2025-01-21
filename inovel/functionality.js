d1d4bac0cc2816b5588b356ed1b61ef7119b882b
Francesco <tallinn123@yahoo.com>
2025-01-21 11:45:20 +0200

    Update to the latest version

--- inovel/functionality.js
+++ inovel/functionality.js
@@ -1,18 +1,5 @@
-const typingSpeed = 35; // Typing speed in milliseconds
-const revealNextParagraph = false;
-const jsonFile = "data.json"; // JSON file containing the story data
-
-// We set language-specific words
-let language = "ita"; // Default language
-let text = [];
-if (language == "ita") {
-	text[0] = "CAPITOLO";
-	text[1] = "Fine.";
-} else if (language == "eng") {
-	text[0] = "CHAPTER";
-	text[1] = "The End.";
-}
-
+let debug = false;
+let typingSpeed = 35;
 let storyData = {};
 let chapterName;
 let chapterTitle;
@@ -20,9 +7,11 @@ let chapterId;
 let activeParagraphId;
 let paragraphsArray = [];
 let typingIntervalId;
+let dynamicVars = {};
 
+// We load the JSON file
 document.addEventListener('DOMContentLoaded', () => {
-	fetch('stories/' + jsonFile)
+	fetch('stories/sample_story.json')
 		.then(response => {
 			if (!response.ok) {
 				throw new Error('Network response was not ok ' + response.statusText);
@@ -38,17 +27,35 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 });
 
-
 function init() {
+	typingSpeed = storyData.story.typingSpeed; // Typing speed in milliseconds
+
+	// We set language-specific words
+	let language = "ita"; // Default language
+	let text = [];
+	if (language == "ita") {
+		text[0] = "CAPITOLO";
+		text[1] = "Fine.";
+	} else if (language == "eng") {
+		text[0] = "CHAPTER";
+		text[1] = "The End.";
+	}
+
 	addResizeListener();
-    addClickPageListener();
+	addClickPageListener();
+
+	// Cycle through the variables array to create an object of dynamic variables
+	let variables = storyData.story.variables;
+	variables.forEach(item => {
+		dynamicVars[item.name] = item.value;
+	});
+
 	container = document.getElementById("text-placeholder");
 	gameOverElement = document.getElementById("game-over");
 	setChapterTitle(0);
 	setParagraph(0, 0);
 	typeText(0);
 }
-
 function addClickPageListener() {
 	document.addEventListener("click", function (event) {
 
@@ -59,14 +66,10 @@ function addClickPageListener() {
 			dropDown.remove();
 		} 
 
-		// Show full paragraph immediately when use clicks anywhere on the page
+		// Show full paragraph immediately when the user clicks anywhere on the page
 		clearInterval(typingIntervalId);
-		let spanElement = document.getElementById("paragraph" + activeParagraphId);
-		let keyword = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].keyword;
-		let paragraphText = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].text_body;
-		let paragraphHtml = highlightKeyword(activeParagraphId, paragraphText, keyword);
-		let paragraphType = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].type;
-		displayFullParagraph(spanElement, paragraphHtml, keyword, activeParagraphId, paragraphType);
+		let pObj = getParagraphObject(activeParagraphId);
+		displayFullParagraph(pObj.spanElement, pObj.paragraphHtml, pObj.keyword, activeParagraphId, pObj.paragraphType);
     });
 }
 
@@ -77,12 +80,10 @@ function setChapterTitle(chapterNum) {
 	document.getElementById("chapterTitle").innerHTML = "<b>" + chapterName + " " + (chapterId+1) + "</b> - " + chapterTitle;
 }
 
-function setParagraph(paragraphNum, choiceNum) {
+function setParagraph(paragraphId) {
 
-	// We populate paragraphsArray with the current and next paragraph ids
-	// let destinationId = storyData.story.chapters[chapterId].paragraphs[paragraphNum].choices[choiceNum].destination_id;
-	// paragraphsArray.push(paragraphNum, destinationId);
-	paragraphsArray.push(paragraphNum);
+	// We populate paragraphsArray with the current paragraph id
+	paragraphsArray.push(paragraphId);
 
 	// We create a span element for each paragraph
 	paragraphsArray.forEach(item => {
@@ -99,34 +100,52 @@ function typeText(pIndex) {
 	displayGameOver(false);
 	setParagraphText(pIndex);
 }
+
+function getParagraphObject(activeParagraphId) {
+	let spanElement = document.getElementById("paragraph" + activeParagraphId);
+
+	// We retrieve the index of the paragraph in the JSON file's paragraph array, based on the paragraphID
+	let pIndexInJSON = getParagraphIndexById(chapterId, activeParagraphId);
+
+	let paragraphText = parseTextForVariables(storyData.story.chapters[chapterId].paragraphs[pIndexInJSON].text_body);
+	let keyword = storyData.story.chapters[chapterId].paragraphs[pIndexInJSON].keyword;
+	let paragraphHtml = highlightKeyword(activeParagraphId, paragraphText, keyword);
+	let paragraphType = storyData.story.chapters[chapterId].paragraphs[pIndexInJSON].type;
+
+	return {
+		"spanElement": spanElement,
+		"paragraphText": paragraphText,
+		"keyword": keyword,
+		"paragraphHtml": paragraphHtml,
+		"paragraphType": paragraphType
+	}
+
+}
 function setParagraphText(pIndex) {
-	console.log(`[setParagraphText] pIndex: ${pIndex}`);
 	if (pIndex < paragraphsArray.length) {
 
 		// We set the paragraph's plain text and html text
 		activeParagraphId = paragraphsArray[pIndex];
-		let spanElement = document.getElementById("paragraph" + activeParagraphId);
-		let paragraphText = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].text_body;
-		let keyword = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].keyword;
-		let paragraphHtml = highlightKeyword(activeParagraphId, paragraphText, keyword);
-		let paragraphType = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].type;
+		let pObj = getParagraphObject(activeParagraphId);
 
 		let i = 0;
 		typingIntervalId = setInterval(() => {
-			// We slice HTML tags
-			if (paragraphText.charAt(i) == "<") {
-				let tag = paragraphText.slice(i, paragraphText.indexOf(">", i) + 1);
-				spanElement.innerHTML += tag;
+            // We slice HTML tags such as <p> and <br> and add them to the span element
+			if (pObj.paragraphText.charAt(i) == "<") {
+				let tag = pObj.paragraphText.slice(i, pObj.paragraphText.indexOf(">", i) + 1);
+				pObj.spanElement.innerHTML += tag;
 				i += tag.length;
 			} else {
-				spanElement.innerHTML += paragraphText.charAt(i);
+				pObj.spanElement.innerHTML += pObj.paragraphText.charAt(i);
 				i++;
+				// Scroll to the bottom after each character is typed
+				if (!isBottomInView()) {
+					window.scrollTo(0, document.body.scrollHeight);
+				}
 			}
-			if (i >= paragraphText.length) {
+			if (i >= pObj.paragraphText.length) {
 				clearInterval(typingIntervalId);
-				displayFullParagraph(spanElement, paragraphHtml, keyword, activeParagraphId, paragraphType);
-				pIndex++;
-				setParagraphText(pIndex);
+				displayFullParagraph(pObj.spanElement, pObj.paragraphHtml, pObj.keyword, activeParagraphId, pObj.paragraphType);
 			}
 		}, typingSpeed);
 	} else {
@@ -134,14 +153,46 @@ function setParagraphText(pIndex) {
 	}
 }
 
+function parseTextForVariables(text) {
+	return text.replace(/{(\w+)}/g, (match, p1) => dynamicVars[p1] || match);
+}
+
+function getParagraphIndexById(chapterId, paragraphId) {
+	return storyData.story.chapters[chapterId].paragraphs.findIndex(paragraph => paragraph.id === paragraphId);
+}
+
 function displayFullParagraph(spanElement, paragraphHtml, keyword, paragraphId, paragraphType) {
-	spanElement.innerHTML = paragraphHtml + " ";
+
+	let parIdLabel = "";
+	if (debug) {
+		parIdLabel = "<p id='debug'>&nbsp;&nbsp;Array Index: <strong>" +
+			paragraphsArray.indexOf(paragraphId) + "</strong>&nbsp;&nbsp;|&nbsp;&nbsp;Paragraph ID: <strong>" +
+			paragraphId + "</strong>&nbsp;&nbsp;|&nbsp;&nbsp;Array: <strong>" +
+			paragraphsArray + "</strong>&nbsp;&nbsp;|&nbsp;&nbsp;Length: <strong>" +
+			paragraphsArray.length + "</strong>&nbsp;&nbsp;|&nbsp;&nbsp;Type: <strong>" +
+			paragraphType + "</strong></p > ";
+	}
+	spanElement.innerHTML = parIdLabel + paragraphHtml + " ";
 	if (keyword) {
 		activateKeyword(paragraphId)
 	};
 	if (paragraphType == "storyEnd") {
-        displayGameOver(true);
+		displayGameOver(true);
+	} else if (paragraphType == "passThru") {
+
+		let destinationId = storyData.story.chapters[chapterId].paragraphs[activeParagraphId].destination_id;
+
+		// 2. Add the new paragraphId (the new destination) to the paragraphsArray
+		paragraphsArray.push(destinationId);
+
+		// 3.  Create a new paragraph container
+		createParagraphContainer(destinationId);
+
+		// 4.  Type the new paragraph on screen
+		typeText(paragraphsArray.length - 1);
 	};
+	// Scroll to the bottom after the full paragraph has been displayed
+	window.scrollTo(0, document.body.scrollHeight);
 }
 
 function displayGameOver(gameOver) {
@@ -160,22 +211,22 @@ function highlightKeyword(paragraphId, paragraphText, keyword) {
 function activateKeyword(paragraphId) {
     let keywordId = "keyword" + paragraphId;
 	const keywordElement = document.getElementById(keywordId);
-	console.log(`keywordId: ${keywordId}`);
 
 	keywordElement.addEventListener("click", function (event) {
 		event.stopPropagation(); // Prevent the event from bubbling up to the document
 		// Check if the dropdown has been already created, if not, create one
 		const dropDownElement = document.getElementById('options');
+		let pIndex = paragraphsArray.indexOf(paragraphId);
 		if (!dropDownElement) {
-			createDropDown(keywordElement, paragraphId);
-        // if a dropdown exists but it's not for the current paragraph, remove it and create a new one
+			createDropDown(keywordElement, pIndex);
+        // if a dropdown exists but it doesn't belong to the current paragraph, remove it and create a new one
 		} else if (dropDownElement.dataset.paragraph_id != paragraphId) {
 			dropDownElement.remove();
-            createDropDown(keywordElement, paragraphId);
+			createDropDown(keywordElement, pIndex);
 		}
 	});
 }
-function selectWord(newWord, keywordElement, paragraphId) {
+function selectWord(newWord, keywordElement, pIndex) {
 	keywordElement.innerHTML = newWord;
 	clearInterval(typingIntervalId);
 
@@ -185,23 +236,31 @@ function selectWord(newWord, keywordElement, paragraphId) {
 		dropDown.remove();
 	}
 
-	// Cycle through the choices to find the destination_id
-	let choices = storyData.story.chapters[chapterId].paragraphs[paragraphId].choices;
+	// 0. Cycle through the choices to find the destination_id
+	// 0.1 We retrieve the index of the paragraph in the JSON file, based on the paragraphID
+	let pIndexInJSON = getParagraphIndexById(chapterId, paragraphsArray[pIndex]);
+	let choices = storyData.story.chapters[chapterId].paragraphs[pIndexInJSON].choices;
 	let destinationId;
 	choices.forEach(item => {
 		if (item.text_body === newWord) {
 			destinationId = item.destination_id;
-		}
+
+			// We set dynamic variables
+			let variables = item.variables;
+			if (variables) {
+				variables.forEach(variable => {
+					dynamicVars[variable.name] = variable.value;
+				});
+			}
+		}	
 	});
 
 	// 1. Remove all the paragraph containers after the current paragraph
-	let arrayIndex = paragraphsArray.indexOf(paragraphId);
-
 	// 1.1 Remove all items after arrayIndex
-	if (arrayIndex !== -1) {
-		paragraphsArray.splice(arrayIndex + 1).forEach(id => {
-			let spanElementId = "paragraph" + id;
-			let spanElement = document.getElementById(spanElementId);
+	if (pIndex !== -1) {
+		let splicedParagraphsArray = paragraphsArray.splice(pIndex + 1);
+		splicedParagraphsArray.forEach(id => {
+			let spanElement = document.getElementById("paragraph" + id);
 			if (spanElement) {
 				spanElement.remove();
 			}
@@ -209,7 +268,7 @@ function selectWord(newWord, keywordElement, paragraphId) {
 	}
 
 	// 1.2 Remove all elements in paragraphsArray after arrayIndex
-	paragraphsArray.splice(arrayIndex + 1);
+	paragraphsArray.splice(pIndex + 1);
 
 	// 2. Add the new paragraphId (the new destination) to the paragraphsArray
     paragraphsArray.push(destinationId);
@@ -221,27 +280,24 @@ function selectWord(newWord, keywordElement, paragraphId) {
 	typeText(paragraphsArray.length - 1);
 }
 
-function positionDropdown(dropdownElement, keywordElement) {
-	dropdownElement.style.left = keywordElement.offsetLeft + 'px';  /* Align the left position */
-	dropdownElement.style.top = keywordElement.offsetTop + keywordElement.offsetHeight + 'px';  /* Position below the interactive word */	
-}
-
-function createDropDown(keywordElement, paragraphId) {
+function createDropDown(keywordElement, pIndex) {
 	
-	let selectedWord = keywordElement.innerHTML;
+	// let selectedWord = keywordElement.innerHTML;
+
+	let parID = paragraphsArray[pIndex];
 	
 	// Create a <ul> element
 	const ul = document.createElement('ul');
 	ul.className = 'options';
 	ul.id = 'options';
 	ul.style.display = 'block';
-	ul.dataset.paragraph_id = paragraphId;
-	positionDropdown(ul, keywordElement);
+	ul.dataset.paragraph_id = parID;
+
+	// We retrieve the index of the paragraph in the JSON file, based on the paragraphID
+	let pIndexInJSON = getParagraphIndexById(chapterId, parID);
 
 	// Create an array of choices
-	let choices = storyData.story.chapters[chapterId].paragraphs[paragraphId].choices;
-
-	console.log(`choices: ${choices}`);
+	let choices = storyData.story.chapters[chapterId].paragraphs[pIndexInJSON].choices;
 
 	let words = [];
     choices.forEach(item => {
@@ -254,15 +310,36 @@ function createDropDown(keywordElement, paragraphId) {
 		const li = document.createElement('li');
 		li.onclick = function (event) {
 			event.stopPropagation();
-			selectWord(item, keywordElement, paragraphId);
+			selectWord(item, keywordElement, pIndex);
 		};		
 		li.textContent = item;
 		ul.appendChild(li);
 		//}
-	});		
+	});	
 
 	// Append the <ul> element to the dropdown-list div
-	document.getElementById('dropdown-list').appendChild(ul);
+	const dropdownList = document.getElementById('dropdown-list');
+	dropdownList.appendChild(ul);
+	positionDropdown(ul, keywordElement);
+	
+}
+function positionDropdown(dropdownElement, keywordElement) {
+	dropdownElement.style.left = keywordElement.offsetLeft + 'px';  /* Align the left position */
+	if (!isBottomInView(dropdownElement)) {
+		// Position above the interactive word
+		dropdownElement.style.top = (keywordElement.offsetTop - dropdownElement.offsetHeight - keywordElement.offsetHeight) + 'px';
+	} else {
+		// Position below the interactive word
+		dropdownElement.style.top = (keywordElement.offsetTop + keywordElement.offsetHeight) + 'px';
+	}
+}
+
+function isBottomInView(element) {
+	if (element) {
+		return (window.innerHeight + window.scrollY + element.offsetHeight) >= document.body.scrollHeight;
+	} else {
+		return (window.innerHeight + window.scrollY) >= document.body.scrollHeight;
+	}
 }
 
 function addResizeListener(){
