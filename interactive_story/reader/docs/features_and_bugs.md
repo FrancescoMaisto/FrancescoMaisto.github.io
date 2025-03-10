de04e1ee821806cdbe96058dea5e17c1978af1f0
Francesco <tallinn123@yahoo.com>
2025-03-10 08:16:40 +0200

    Adding interactive story project

---
+++ interactive_story/reader/docs/features_and_bugs.md
@@ -0,0 +1,35 @@
+
+# Features
+
+✅ When the player clicks anywhere on the screen, the paragraph text is immediately replaced by the HTML txt.
+✅ Ability to load a JSON file locally
+✅ Add to Git
+✅ Declaring, Setting and Getting variables
+✅ Pass-through paragraphs
+✅ Ability to add an image to a paragraph.
+✅ Ability to choose between different themes
+✅ Ability to have the same paragraph displayed multiple times in the story
+✅ INVENTORY: The player can have an inventory of items
+✅ Undo array: ability to undo all variables changes when stepping back.
+✅ Dynamic creation of navbar based on story's variables
+✅ Ability to change numeric variables
+✅ Conditional destinations based on whether the player has a certain item (i.e. a key)
+✅ Make sure each Undo Array entry is an array of objects, as opposed to a single object per entry.
+✅ Each paragraph should have an extra property that dictates whether the paragraph is typed letter by letter or shown immediately. (I have done this using paragraph type "infoBox" for now)
+✅ Language choice screen
+✅ Programmatically create screens layouts (language screen, story screen)
+✅ Add a visual hint of where to click/tap on a keyword
+ 
+🔲 Conditional destinations based on numeric variables (<,>, =)
+🔲 Conditional destinations based on whether the player has visited a certain paragraph
+🔲 Iron Man mode. The player cannot go back to previous paragraphs
+🔲 Multiple Chapters
+🔲 Multiple Stories (Main Menu)
+🔲 Settings menu (typing speed/theme/debugmode/show images?)
+
+# Bugs
+
+✅ When the same paragraph shows more than once in the story, the choices dropdown doesn't open anymore
+✅ When resizing window the drop down sticks to the wrong keyword
+✅ The inventory dropdown should stick to the bottom o the navbar
+✅ Inventory resets when I get a second inventory item
---
+++ interactive_story/reader/js/inventory.js
@@ -0,0 +1,72 @@
+﻿let inventoryArray = []; // An array containing all inventory items
+function toggleInventory(show) {
+    const invDiv = document.getElementById('inventory');
+    const inventoryButton = document.getElementById('inventory-button');
+    const guiElement = document.getElementById('gui');
+    const navBar = document.getElementById('navbar');
+    const position = show ? navBar.offsetHeight + guiElement.offsetHeight : navBar.offsetHeight - guiElement.offsetHeight;
+
+    if (show) {
+        console.log("[toggleInventory] Showing inventory panel");
+        inventoryButton.textContent = text[2] + " ▼";
+        inventoryButton.className = "active";
+
+        const newInvDiv = document.createElement('div');
+        newInvDiv.id = 'inventory';
+
+        if (inventoryArray.length <= 0) {
+            const emptyLabel = document.createElement('p');
+            emptyLabel.textContent = text[3];
+            emptyLabel.className = 'inventory-empty';
+            newInvDiv.appendChild(emptyLabel);
+        } else {
+            populateInventory(newInvDiv);
+        }
+
+        document.getElementById('gui').appendChild(newInvDiv);
+    } else {
+        console.log("[toggleInventory] Hiding inventory panel");
+        inventoryButton.textContent = text[2] + " ▲";
+        inventoryButton.className = "";
+        setTimeout(() => invDiv.remove(), 500); // Hide the GUI element after a short delay
+    }
+
+    guiElement.style.top = position + 'px';
+}
+function showInventory() {
+    let invDiv = document.getElementById('inventory');
+    toggleInventory(!invDiv);
+}
+function createInventoryPanel() {
+    toggleInventory(true);
+}function playerHasItemInInventory(itemName) {
+    return inventoryArray.includes(itemName);
+}
+function populateInventory(invDiv) {
+    // Iterates over the inventory array to create the items elements
+    inventoryArray.forEach(item => {
+        const i = document.createElement('a');
+        i.className = 'inventory-item';
+        i.textContent = item;
+        i.href = "javascript:void(0)";
+        i.onclick = function (event) {
+            event.stopPropagation();
+        };
+        invDiv.appendChild(i);
+    });
+}
+function refreshInventory() {
+    // Updates the inventory content in case the inventory is visible when the player picks up an item
+    const invDiv = document.getElementById('inventory');
+    if (invDiv) {
+        invDiv.remove();
+        createInventoryPanel();
+    }
+}
+function removeItemFromInventory(item) {
+    const index = inventoryArray.indexOf(item.value);
+    if (index !== -1) {
+        inventoryArray.splice(index, 1);
+        console.log(`[removeItemFromInventory] Removed inventory item: ${item.value}`);
+    }
+}
